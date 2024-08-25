d7d4c2b531048a3dd771290f2165ca5bc6ac00aa
mshaberle <millershaberle+github@gmail.com>
2024-08-25 03:37:31 -0500

    Fix binding qBittorrent port, consolidate formatting and silencing

--- port-refresh.sh
+++ port-refresh.sh
@@ -7,18 +7,17 @@ GLUETUN_URL="http://$GLUETUN_SERVER:$GLUETUN_PORT/v1/openvpn/portforwarded"
 QBITTORRENT_URL="http://$QBITTORRENT_SERVER:$QBITTORRENT_PORT/api/v2"
 
 function GetQBitPort {
-	qBitPort=$(curl -s -S -b ${COOKIES} "$QBITTORRENT_URL/app/preferences" | jq -r '.listen_port')
+	qBitPort=$(curl -s -S -b ${COOKIES} ${QBITTORRENT_URL}/app/preferences | jq -r '.listen_port')
 	echo "Got qBittorrent port! (${qBitPort})"
 }
 
 function SetQBitPort {
-	curl -s -S -b ${COOKIES} --data 'json={"listen_port: "'"$gluetunPort"'"}' "$QBITTORRENT_URL/app/setPreferences"
+	curl -s -S -b ${COOKIES} --data 'json={"listen_port":"'"$gluetunPort"'"}' ${QBITTORRENT_URL}/app/setPreferences
 	echo "Changed qBittorrent port to ${gluetunPort}!"
 }
 
 function GetSID {
-	curl -s -S -c ${COOKIES} --data "username=$QBITTORRENT_USERNAME&password=$QBITTORRENT_PASSWORD" ${QBITTORRENT_URL}/auth/login > /dev/null
-	#Need to to something here to stop the curl output
+	curl -s -S -c ${COOKIES} --data "username=$QBITTORRENT_USERNAME&password=$QBITTORRENT_PASSWORD" ${QBITTORRENT_URL}/auth/login
 	
 	if grep -q SID ${COOKIES}; then
 		echo "Successfully logged in to qBittorrent!"
